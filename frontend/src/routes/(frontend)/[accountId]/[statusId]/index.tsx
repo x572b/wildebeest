0d0622133b71b0fe3bde16fd04cfc77ea52bdddf
Dario Piotrowicz <dario@cloudflare.com>
2023-02-15 21:07:26 +0000

    split account index.tsx into layout.tsx + index.tsx
    perform such split so that we display the posts in the
    index.tsx file and this will allow us to also show different
    tabs on the page as well

--- frontend/src/routes/(frontend)/[accountId]/[statusId]/index.tsx
+++ frontend/src/routes/(frontend)/[accountId]/[statusId]/index.tsx
@@ -6,7 +6,6 @@ import { formatRoundedNumber } from '~/utils/numbers'
 import * as statusAPI from 'wildebeest/functions/api/v1/statuses/[id]'
 import * as contextAPI from 'wildebeest/functions/api/v1/statuses/[id]/context'
 import { DocumentHead, Link, loader$ } from '@builder.io/qwik-city'
-import StickyHeader from '~/components/StickyHeader/StickyHeader'
 import { Avatar } from '~/components/avatar'
 import { MediaGallery } from '~/components/MediaGallery.tsx'
 import { getNotFoundHtml } from '~/utils/getNotFoundHtml/getNotFoundHtml'
@@ -55,7 +54,6 @@ export default component$(() => {
 
 	return (
 		<>
-			<StickyHeader withBackButton />
 			<div class="bg-wildebeest-700 p-4">
 				<AccountCard status={loaderData.status} />
 				<div class="leading-normal inner-html-content text-lg" dangerouslySetInnerHTML={loaderData.status.content} />
